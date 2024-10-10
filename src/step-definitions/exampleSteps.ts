3f77ee011ea21083aed5ee9d9293d1b7db58c78a
elifkanlioglu <elifkanlioglu@gmail.com>
2024-10-10 12:02:27 +0100

    exampleSteps

--- src/step-definitions/exampleSteps.ts
+++ src/step-definitions/exampleSteps.ts
@@ -4,7 +4,6 @@ import { page } from '../step-definitions/hooks'; // Import the page object
 
 Given('I navigate to {string}', async (url: string) => {
   await page.goto(url);
-  await page.goto(url);
 });
 
 Then('the page title should be {string}', async (expectedTitle: string) => {
